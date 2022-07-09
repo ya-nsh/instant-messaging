f06ba16fb0cbe365977a20bbff376406ea8b9b3f
ya-nsh <yansh2017@gmail.com>
2022-07-09 11:16:52 +0530

    add chat endpoint and sample data

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const { chats } = require('./data/data');
 
 const app = express();
+dotenv.config(); // Loads environment variables from .env file
 
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api/chat', (req, res) => {
+  console.log(chats);
+  res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+  console.log(req.params.id);
+  const filteredChat = chats.filter(chat => chat._id === req.params.id);
+  res.send(filteredChat);
 });
 
 app.listen(port, console.log(`listening on port ${port}`));
