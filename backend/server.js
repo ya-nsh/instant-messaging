190a862e322c7f07c025d96cc395a8fd9e99c347
ya-nsh <yansh2017@gmail.com>
2022-07-11 17:18:35 +0530

    add chatRoutes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { chats } = require('./data/data');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const chatRoutes = require('./routes/chatRoutes');
 
 const app = express();
 app.use(express.json()); // for parsing application/json
@@ -13,16 +14,8 @@ connectDB(); // Connects to MongoDB
 
 const port = process.env.PORT || 5000;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// app.get('/api/chat', (req, res) => {
-//   console.log(chats);
-//   res.send(chats);
-// });
-
 app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
